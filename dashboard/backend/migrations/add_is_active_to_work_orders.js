afcfb38dcde9924b2d83b1a96c64a2faced64f64
Rachel Marvin Wan <121346040+RachelMarvinWan@users.noreply.github.com>
2025-09-30 04:49:16 +0800

    Add rejectUnauthorized option to SSL configuration

--- dashboard/backend/migrations/add_is_active_to_work_orders.js
+++ dashboard/backend/migrations/add_is_active_to_work_orders.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require ('fs');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,7 +8,9 @@ const pool = new Pool({
 	database: process.env.DB_NAME,
 	password: process.env.DB_PASSWORD,
 	port: process.env.DB_PORT,
-	ssl: { ca: fs.readFileSync('/home/ubuntu/rds-certs/ap-southeast-5-bundle.pem','utf8')}
+	ssl: { ca: fs.readFileSync('/home/ubuntu/rds-certs/ap-southeast-5-bundle.pem','utf8'),
+		 rejectUnauthorized: true
+		 }
 });
 
 (async () => {
